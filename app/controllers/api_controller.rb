d9adea3c562f4d15f022a0de9e4a395a4da5298f
Matthew Barnard <msbarnard10@gmail.com>
2016-07-09 22:11:45 +0200

    fixed rendering issues

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,15 +1,23 @@
 class ApiController < ApplicationController
+  before_action :authenticate, except: :connect
   after_action :set_access_control_headers
 
   def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = "*"
-    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(',')
   end
   # ----------------------------------------
-  # API TEST
+  # API CONNECTION
   # ----------------------------------------
   def connect
-    render plain: 'Hello from the API :D', status: 200
+    user = nil
+    if params[:api_key].blank?
+      user = User.new
+      user.save
+    else
+      user = authenticate
+    end
+    render plain: user.call_id, status: 201
   end
 
   # ----------------------------------------
@@ -18,4 +26,15 @@ class ApiController < ApplicationController
   def trello_login
     render plain: 'Hey :D'
   end
+
+
+  def authenticate
+    authentication_user = User.find_by call_id: params[:api_key]
+    if authentication_user.blank?
+      render plain: 'Not Authenticated', status: 401 and return
+    else
+      authentication_user
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@ Rails.application.routes.draw do
   scope '/api' do
     scope '/v1' do
       get '/connect' => 'api#connect'
-      scope '/trello' do
-        post '/authenticate' => 'api#trello_login'
+      scope ':api_key' do
+        get '/connect' => 'api#connect'
+        scope '/trello' do
+          post '/authenticate' => 'api#trello_login'
+        end
       end
     end
   end
 end
+
